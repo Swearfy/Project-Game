bde4f1ac91620fd566b08fa3d97a629a49532174
Dennis Paul Puscasu <swearfy@gmail.com>
2022-10-23 17:05:56 +0100

    check for out of bound collision

--- player.js
+++ player.js
@@ -5,12 +5,28 @@ export class Player {
     this.height = 50;
     this.x = this.game.width / 2 - this.width / 2;
     this.y = this.game.height / 2 - this.height / 2;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.maxSpeed = 2;
   }
   update(input) {
-    if (input.includes("d")) this.x += 1.2;
-    else if (input.includes("s")) this.y += 1.2;
-    else if (input.includes("a")) this.x -= 1.2;
-    else if (input.includes("w")) this.y -= 1.2;
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (input.includes("d")) this.speedX = this.maxSpeed;
+    else if (input.includes("a")) this.speedX = -this.maxSpeed;
+    else this.speedX = 0;
+
+    if (input.includes("s")) this.speedY = this.maxSpeed;
+    else if (input.includes("w")) this.speedY = -this.maxSpeed;
+    else this.speedY = 0;
+
+    if (this.x < 0) this.x = 0;
+    if (this.x > this.game.width - this.width)
+      this.x = this.game.width - this.width;
+
+    if (this.y < 0) this.y = 0;
+    if (this.y > this.game.height - this.height)
+      this.y = this.game.height - this.height;
   }
   draw(context) {
     context.fillRect(this.x, this.y, this.width, this.height);
