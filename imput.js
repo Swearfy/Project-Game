0eaeee53fac4aba3f87854f4071cbd1ac192e290
Dennis Paul Puscasu <swearfy@gmail.com>
2022-10-23 21:01:45 +0100

    fixed aim relative to canvas

--- imput.js
+++ imput.js
@@ -1,3 +1,4 @@
+const canvas = document.getElementById("canvas1");
 export class InputHandle {
   constructor(game) {
     this.game = game;
@@ -22,9 +23,20 @@ export class InputHandle {
       console.log(e.key, this.keys);
     });
 
+    // get the accourate mouse position based on canvas coordinates
+    function getMousePos(canvas, evt) {
+      var rect = canvas.getBoundingClientRect();
+      return {
+        x:
+          ((evt.clientX - rect.left) / (rect.right - rect.left)) * canvas.width,
+        y:
+          ((evt.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height,
+      };
+    }
     window.addEventListener("click", (e) => {
-      this.clickX = e.clientX;
-      this.clickY = e.clientY;
+      getMousePos(canvas, e);
+      this.clickX = getMousePos(canvas, e).x;
+      this.clickY = getMousePos(canvas, e).y;
       this.game.player.shoot(this.clickX, this.clickY);
     });
   }
