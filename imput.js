f149d4ff717ac54edb634f3d70f15ef67854d4cd
Dennis Paul Puscasu <swearfy@gmail.com>
2022-10-30 16:38:07 +0000

    adjustments

--- imput.js
+++ imput.js
@@ -10,15 +10,12 @@ export class InputHandle {
       ) {
         this.keys.push(e.key);
       }
-      console.log(e.key, this.keys);
     });
 
     window.addEventListener("keyup", (e) => {
-      console.log(e.key, this.keys);
       if (e.key === "w" || e.key === "a" || e.key === "s" || e.key === "d") {
         this.keys.splice(this.keys.indexOf(e.key), 1);
       }
-      console.log(e.key, this.keys);
     });
 
     // get the accourate mouse position based on canvas coordinates
